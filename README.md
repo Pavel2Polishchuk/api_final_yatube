da1e588078e03cafa597a21a88ade9c0c2f289df
Павел Полищук <stiksxdi@yandex.ru>
2023-02-22 18:42:33 +0300

    Готовый проект

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# api_final
-api final
+**Как запустить проект:**
+Клонировать репозиторий и перейти в него в командной строке:
+```
+git clone git@github.com:Pavel2Polishchuk/api_final_yatube.git
+```
+```
+cd kittygram
+```
+
+Cоздать и активировать виртуальное окружение:
+
+```
+python -m venv env
+source env/bin/activate
+```
+Установить зависимости из файла requirements.txt:
+
+```
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+Выполнить миграции:
+
+```
+python manage.py migrate
+```
+Запустить проект:
+
+```
+python3 manage.py runserver
+```
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
     'api',
     'posts',
 ]
@@ -77,7 +78,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -91,9 +92,18 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+}
+
+SIMPLE_JWT = {
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+   'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
